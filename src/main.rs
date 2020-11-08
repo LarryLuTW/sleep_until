ba588b6603b3a42952aae32278c529e38edc2de9
Larry850806 <pudding850806@gmail.com>
2020-11-08 20:23:05 +0800

    Add usage

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,19 @@ use std::env;
 use std::thread;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args: Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("usage: sleep_until 20:15:00");
+        return;
+    }
+
     let end_str = &args[1]; // "20:01:00"
-    let end = NaiveTime::parse_from_str(end_str, "%H:%M:%S").unwrap();
+    let end = NaiveTime::parse_from_str(&end_str, "%H:%M:%S").unwrap();
 
     let now = Local::now();
     let begin = NaiveTime::from_hms(now.hour(), now.minute(), now.second());
 
-    println!("{} - {}", begin, end);
+    println!("From {} to {}", begin, end);
 
     let dur = end.signed_duration_since(begin).to_std().unwrap();
     thread::sleep(dur);
